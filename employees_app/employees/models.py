ca07bfb37d7658d9cf5d38817cbac3fbb44fb157
Violeta Kalcheva <violetakalcheva@abv.bg>
2022-02-04 17:39:16 +0200

    Adding new column in table employee

--- employees_app/employees/models.py
+++ employees_app/employees/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=30,)
-    last_name = models.CharField(max_length=40,null=True,blank=True,default='NO NAME',)
-    egn = models.CharField(max_length=10,unique=True,)
+    first_name = models.CharField(max_length=30, )
+    last_name = models.CharField(max_length=40, null=True, blank=True, default='NO NAME', )
+    egn = models.CharField(max_length=10, unique=True, )
+    job_title = models.CharField(
+        max_length=40,
+        choices=(
+            ('Software developer', 1),
+            ('QA Engineer', 2),
+            ('DevOps', 3),
+        )
+    )
